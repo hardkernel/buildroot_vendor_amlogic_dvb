d0d16e0410a14352c35f04e30da208fb4febbfdb
Gong Ke <ke.gong@amlogic.com>
2011-12-23 12:44:24 +0800

    fixed Android.mk for ics-amlogic

--- android/ex_lib/Android.mk
+++ android/ex_lib/Android.mk
@@ -1,5 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 	
-$(call add-prebuilt-files, STATIC_LIBRARIES, libfreetype.a) 
-$(call add-prebuilt-files, STATIC_LIBRARIES, libiconv.a)
+#$(call add-prebuilt-files, STATIC_LIBRARIES, libfreetype.a) 
+#$(call add-prebuilt-files, STATIC_LIBRARIES, libiconv.a)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libfreetype.a
+LOCAL_BUILT_MODULE_STEM := libfreetype.a
+LOCAL_MODULE_SUFFIX := a
+LOCAL_MODULE := libfreetype
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libiconv.a
+LOCAL_BUILT_MODULE_STEM := libiconv.a
+LOCAL_MODULE_SUFFIX := a
+LOCAL_MODULE := libiconv
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+include $(BUILD_PREBUILT)
+
