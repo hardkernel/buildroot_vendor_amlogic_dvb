392ffc6056e2ffa31d5cff81f61ff884ed52032b
jiangzhongming <jiangzhongming@droid02-bj.(none)>
2012-05-07 10:29:18 +0800

    add am fend ctrl module

--- include/am_adp/am_fend_diseqc_cmd.h
+++ include/am_adp/am_fend_diseqc_cmd.h
@@ -8,8 +8,8 @@
  * \date 2012-03-20: create the document
  ***************************************************************************/
 
-#ifndef _AM_FEND_H
-#define _AM_FEND_H
+#ifndef _AM_FEND_DISEQC_CMD_H
+#define _AM_FEND_DISEQC_CMD_H
 
 #include "am_types.h"
 #include "am_evt.h"
@@ -61,14 +61,16 @@ typedef enum AM_FEND_SWITCHINPUT
 typedef enum AM_FEND_POLARISATION
 {
 	AM_FEND_POLARISATION_V,
-	AM_FEND_POLARISATION_H
+	AM_FEND_POLARISATION_H,
+	AM_FEND_POLARISATION_NOSET
 }AM_FEND_Polarisation_t; 
 
 /**\brief DVB-S/S2前端本振频率*/
 typedef enum AM_FEND_LOCALOSCILLATORFREQ
 {
 	AM_FEND_LOCALOSCILLATORFREQ_L,
-	AM_FEND_LOCALOSCILLATORFREQ_H
+	AM_FEND_LOCALOSCILLATORFREQ_H,
+	AM_FEND_LOCALOSCILLATORFREQ_NOSET
 }AM_FEND_Localoscollatorfreq_t;
 
 /**\brief DVB-S/S2前端本振频率表LOCAL OSCILLATOR FREQ TABLE*/
