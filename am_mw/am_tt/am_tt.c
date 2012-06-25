4bd970d9bd4e7b724c08b8b133fffe167fc41d6b
ke.gong <ke.gong@amlogic.com>
2012-06-25 19:14:52 +0800

    add condition veriable in teletext thread

--- am_mw/am_tt/am_tt.c
+++ am_mw/am_tt/am_tt.c
@@ -107,6 +107,7 @@ typedef struct am_mw_teletext_context_s
 	AM_TT_ClearDisplayCb_t       co;
 	int fhandle;
 	pthread_mutex_t rd_wr_Mutex;
+	pthread_cond_t  rd_wr_Cond;
 	char auto_page_status ;
 	char TtxCodepage;
 	AM_TT_EventCb_t ecb;
@@ -657,9 +658,13 @@ static void* teletext_thread(void *para)
 
 	while(context_tele.flags&TELTE_RUNNING_FLAG)
 	{
-		pthread_mutex_lock(&context_tele.rd_wr_Mutex);	
+		pthread_mutex_lock(&context_tele.rd_wr_Mutex);
+		while((context_tele.flags&TELTE_RUNNING_FLAG) && (context_tele.pwb.rd_ptr==context_tele.pwb.wr_ptr)){
+			pthread_cond_wait(&context_tele.rd_wr_Cond, &context_tele.rd_wr_Mutex);
+		}
 		pbt=get_subtitle_pes_packet(&context_tele.pwb);
 		pthread_mutex_unlock(&context_tele.rd_wr_Mutex);
+
 		if(context_tele.pwb.rd_ptr>context_tele.pwb.wr_ptr)
 			context_tele.pwb.last_rd_wr=1;
 		else
@@ -682,10 +687,6 @@ static void* teletext_thread(void *para)
 			free(pbt);
 
 			continue;
-		}
-		else
-		{
-			usleep(100);
 		}
 	}
 	if(buffer)
@@ -1106,6 +1107,8 @@ AM_ErrorCode_t AM_TT_Init(int buffer_size)
 	Mosaic_RegisterCallback(teletext_fill_rectangle,teletext_convert_color);
 
 	pthread_mutex_init(&context_tele.rd_wr_Mutex,0);
+	pthread_cond_init(&context_tele.rd_wr_Cond,0);
+
 	context_tele.flags=0;
 	context_tele.status=TELETEXT_INILIZED;
 	context_tele.fr=NULL;
@@ -1283,13 +1286,9 @@ static void pes_filter_callback(int dev_no,int fhandle,const unsigned char *data
 	{
 		data_copy+=copy_data_to_pes_buffer((char*)data+data_copy,data_remain);
 		data_remain-=data_copy;
-		if(data_remain>0)
-		{
-			usleep(100);
-		}
-
 	}
 	pthread_mutex_unlock(&context_tele.rd_wr_Mutex);
+	pthread_cond_signal(&context_tele.rd_wr_Cond);
 }
 
 static int open_demux_pes_filter(int dev_no,int pid,int type)
@@ -1441,6 +1440,7 @@ AM_ErrorCode_t AM_TT_Quit()
 	Vtdrawer_deinit();
 	Mosaic_deinit();
 	pthread_mutex_destroy(&context_tele.rd_wr_Mutex);
+	pthread_cond_destroy(&context_tele.rd_wr_Cond);
 	
 	return 0;
 }
@@ -1458,6 +1458,7 @@ AM_ErrorCode_t AM_TT_Stop()
 	if (context_tele.flags&TELTE_RUNNING_FLAG)
 	{
 		context_tele.flags&=~TELTE_RUNNING_FLAG;
+		pthread_cond_signal(&context_tele.rd_wr_Cond);
 		pthread_join(context_tele.teletext_thread,NULL);
 	}
 	AM_DEBUG(1,"@@ teletext thread exit ok");
