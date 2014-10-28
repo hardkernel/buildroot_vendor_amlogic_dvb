e838c248150abe0956c9ed51eb46a67a7d2aca74
ke.gong <ke.gong@amlogic.com>
2014-10-28 10:05:31 +0800

    PD #97420: Add h264 close caption proc function
    Squashed commit of the following:
    
    commit 2055299c2397a6f2d07311871cdfa419bf77d6b0
    Author: pengfei.liu <pengfei.liu@amlogic.com>
    Date:   Thu Oct 23 23:20:56 2014 -0400
    
        PD #97420: fixed swap byte order problem, it will lead to slice_type error.
    
    commit 259252dd7559daf35f1d7bee6f42ac4a37c13199
    Author: pengfei.liu <pengfei.liu@amlogic.com>
    Date:   Tue Oct 21 02:58:55 2014 -0400
    
        PD #97420: Add h264 close caption proc function

--- am_adp/am_userdata/aml/aml.c
+++ am_adp/am_userdata/aml/aml.c
@@ -21,10 +21,21 @@
 #include <string.h>
 #include <errno.h>
 #include <poll.h>
-
+#include <sys/ioctl.h>
 /****************************************************************************
  * Type definitions
  ***************************************************************************/
+#define AMSTREAM_IOC_MAGIC  'S'
+#define AMSTREAM_IOC_UD_PICTYPE _IOR(AMSTREAM_IOC_MAGIC, 0x55, unsigned long)
+#define AMSTREAM_IOC_UD_LENGTH _IOR(AMSTREAM_IOC_MAGIC, 0x54, unsigned long)
+
+#define DMA_BLOCK_LEN 8
+
+enum user_data_type {
+	INVALID_TYPE = 0,
+	MPEG_CC_TYPE = 1,
+	H264_CC_TYPE = 2
+};
 
 enum picture_coding_type {
 	/* 0 forbidden */
@@ -98,7 +109,7 @@ static void aml_reorder_decode_cc_data	(struct aml_ud_reorder *vd,
 {
 	aml_ud_drv_data_t *drv_data = vd->drv_data;
 	AM_USERDATA_Device_t *dev = drv_data->dev;
-	
+
 	n_bytes = AM_MIN (n_bytes, (unsigned int)
 		       sizeof (vd->reorder_buffer[0]));
 
@@ -108,13 +119,13 @@ static void aml_reorder_decode_cc_data	(struct aml_ud_reorder *vd,
 		if (0 != vd->reorder_pictures) {
 			if (vd->reorder_pictures & 5) {
 				/* Top field or top and bottom field. */
-				dev->write_package(dev, 
+				dev->write_package(dev,
 						vd->reorder_buffer[0],
 						vd->reorder_n_bytes[0]);
 			}
 			if (vd->reorder_pictures & 2) {
 				/* Bottom field. */
-				dev->write_package(dev, 
+				dev->write_package(dev,
 						vd->reorder_buffer[1],
 						vd->reorder_n_bytes[1]);
 			}
@@ -152,12 +163,12 @@ static void aml_reorder_decode_cc_data	(struct aml_ud_reorder *vd,
 		if (vd->reorder_pictures >= 3) {
 			if (vd->reorder_pictures >= 4) {
 				/* Top and bottom field. */
-				dev->write_package(dev, 
+				dev->write_package(dev,
 						vd->reorder_buffer[0],
 						vd->reorder_n_bytes[0]);
 			} else {
 				/* Bottom field. */
-				dev->write_package(dev, 
+				dev->write_package(dev,
 						vd->reorder_buffer[1],
 						vd->reorder_n_bytes[1]);
 			}
@@ -221,7 +232,7 @@ static void aml_reorder_user_data			(struct aml_ud_reorder *vd,
 	}
 }
 
-typedef struct 
+typedef struct
 {
 	uint32_t picture_structure:16;
 	uint32_t temporal_reference:10;
@@ -251,7 +262,7 @@ static int aml_extract_package(uint8_t *user_data, int ud_size, uint8_t *cc_data
 		cc_data[cs++] = (_p)[1];\
 		cc_data[cs++] = (_p)[0];\
 	AM_MACRO_END
-	
+
 	*cc_size = 0;
 	if (ud_size < 12)
 		return left_size;
@@ -267,12 +278,12 @@ static int aml_extract_package(uint8_t *user_data, int ud_size, uint8_t *cc_data
 
 				COPY_4_BYTES((uint8_t*)&uh->atsc_id);
 				COPY_4_BYTES(uh->cc_data_start);
-				
+
 				left_size -= sizeof(aml_ud_header_t);
 				p += sizeof(aml_ud_header_t);
 
 				*pkg_header = *uh;
-				
+
 				AM_DEBUG(2, "package index %d", uh->index);
 				continue;
 			}
@@ -283,7 +294,7 @@ static int aml_extract_package(uint8_t *user_data, int ud_size, uint8_t *cc_data
 				break;
 			}
 		}
-		
+
 		if (atsc_id_count == 1)
 		{
 			/* Copy cc data */
@@ -294,7 +305,7 @@ static int aml_extract_package(uint8_t *user_data, int ud_size, uint8_t *cc_data
 				if (uh->atsc_id != 0x47413934)
 					COPY_4_BYTES(p+4);
 			}
-			
+
 			COPY_4_BYTES(p);
 
 			copied = cs - copied;
@@ -313,41 +324,110 @@ static int aml_extract_package(uint8_t *user_data, int ud_size, uint8_t *cc_data
 		cs = 0;
 		left_size = ud_size;
 	}
-	
+
 	*cc_size = cs;
-	
+
 	return left_size;
 }
 
 static void dump_cc_data(uint8_t *buff, int size)
 {
-	int i;
-	char buf[4096];
+	AM_DEBUG(1,  "-------------------------------------------\n");
+	AM_DEBUG(1,  "read %d bytes data:\n", size);
 
-	if (size > 1024)
-		size = 1024;
-	for (i=0; i<size; i++)
+	int i = 0;
+	for (i=0; i<size; )
 	{
-		sprintf(buf+i*3, "%02x ", buff[i]);
+		AM_DEBUG(1, "%02x %02x %02x %02x", buff[i], buff[i+1], buff[i+2], buff[i+3]);
+		i += 4;
+	}
+	AM_DEBUG(1,  "-------------------------------------------\n");
+}
+static void aml_swap_data(uint8_t *user_data, int ud_size)
+{
+	int swap_blocks, i, j, k, m;
+    unsigned char c_temp;
+
+	/* swap byte order */
+    swap_blocks = ud_size / 8;
+    for (i=0; i<swap_blocks; i++) {
+        j = i * 8;
+        k = j + 7;
+        for (m=0; m<4; m++) {
+            c_temp = user_data[j];
+            user_data[j++] = user_data[k];
+            user_data[k--] = c_temp;
+        }
+    }
+}
+static int aml_check_userdata_format(uint8_t *buf, int len)
+{
+	AM_DEBUG(1,"check format len:%d", len);
+	if(len <= 0)
+		return INVALID_TYPE;
+	uint8_t *tmp_buf = (uint8_t *)malloc(len);
+	if(tmp_buf == NULL)
+		return INVALID_TYPE;
+	memcpy(tmp_buf, buf, len);
+	aml_swap_data(tmp_buf, len);
+	if(tmp_buf[0] == 0xb5 && tmp_buf[3] == 0x47 && tmp_buf[4] == 0x41
+			&& tmp_buf[5] == 0x39 && tmp_buf[6] == 0x34)
+	{
+		AM_DEBUG(1,"check format is h264_cc_type");
+		free(tmp_buf);
+		return H264_CC_TYPE;
+	}
+	else
+	{
+		AM_DEBUG(1,"check format is mpeg_cc_type");
+		free(tmp_buf);
+		return MPEG_CC_TYPE;
 	}
 
-	AM_DEBUG(1, "%s", buf);
 }
-
+static unsigned int aml_get_pic_type(unsigned int slice_type)
+{
+	unsigned int pic_type = 0;
+	switch( slice_type&0xFF )
+    {
+	    case 0: case 5:
+	        pic_type = P_TYPE;
+	        break;
+	    case 1: case 6:
+	        pic_type = B_TYPE;
+	        break;
+	    case 2: case 7:
+	        pic_type = I_TYPE;
+	        break;
+	    case 3: case 8: // SP
+	        pic_type = P_TYPE;
+	        break;
+	    case 4: case 9:
+	        pic_type = I_TYPE;
+	        break;
+	    default:
+	        pic_type = I_TYPE;
+	        break;
+    }
+    AM_DEBUG(3, "get slice_type:%d, slice_id:%d, pic_type:%d", slice_type&0xFF, slice_type>>8, pic_type);
+    return pic_type;
+}
 /**\brief CC data thread*/
 static void *aml_userdata_thread(void *arg)
 {
 	AM_USERDATA_Device_t *dev = (AM_USERDATA_Device_t*)arg;
 	aml_ud_drv_data_t *drv_data = (aml_ud_drv_data_t*)dev->drv_data;
 	uint8_t buf[5*1024];
+	uint8_t *p;
 	uint8_t cc_data[256];/*In fact, 99 is enough*/
 	uint8_t cc_data_cnt;
-	int cnt, left, fd;
+	int cnt, left, fd, ud_format = INVALID_TYPE, min_bytes_valid = 0;
 	struct pollfd fds;
 	aml_ud_header_t pheader;
 	struct aml_ud_reorder reorder;
 	int last_pkg_idx = -1;
-	
+	unsigned int slice_type, pic_type;
+
     AM_DEBUG(1, "user data thread start.");
 
 	fd = drv_data->fd;
@@ -356,7 +436,7 @@ static void *aml_userdata_thread(void *arg)
 
 	memset(&reorder, 0, sizeof(reorder));
 	memset(&pheader, 0, sizeof(pheader));
-	
+
 	while (drv_data->running)
 	{
 		fds.events = POLLIN|POLLERR;
@@ -364,49 +444,79 @@ static void *aml_userdata_thread(void *arg)
 		if (poll(&fds, 1, 100) > 0)
 		{
 			cnt = read(fd, buf+left, sizeof(buf)-left);
+			while(ud_format == INVALID_TYPE && drv_data->running)
+			{
+				ud_format = aml_check_userdata_format(buf, cnt);
+			}
 			if (cnt > 0)
 			{
 				AM_DEBUG(3, "read %d bytes user_data", cnt);
-#if 1
-				cnt += left;
-				do
+				if(ud_format == MPEG_CC_TYPE)
 				{
-					/* try to read a package */
-					left = aml_extract_package(buf, cnt, cc_data, &cc_data_cnt, &pheader);
-					if (cc_data_cnt > 0)
+					cnt += left;
+					do
 					{
-						if ((last_pkg_idx + 1) != pheader.index)
+						/* try to read a package */
+						left = aml_extract_package(buf, cnt, cc_data, &cc_data_cnt, &pheader);
+						if (cc_data_cnt > 0)
 						{
-							AM_DEBUG(1, "Warning: package index discontinue, %d->%d", 
-								last_pkg_idx, pheader.index);
+							if ((last_pkg_idx + 1) != pheader.index)
+							{
+								AM_DEBUG(1, "Warning: package index discontinue, %d->%d",
+									last_pkg_idx, pheader.index);
+							}
+
+							reorder.drv_data = drv_data;
+							reorder.picture_coding_type = pheader.picture_coding_type;
+							reorder.picture_structure = pheader.picture_structure;
+							reorder.picture_temporal_reference = pheader.temporal_reference;
+
+							aml_reorder_user_data(&reorder, cc_data, cc_data_cnt);
+
+							last_pkg_idx = pheader.index;
 						}
-						
-						reorder.drv_data = drv_data;
-						reorder.picture_coding_type = pheader.picture_coding_type;
-						reorder.picture_structure = pheader.picture_structure;
-						reorder.picture_temporal_reference = pheader.temporal_reference;
 
-						aml_reorder_user_data(&reorder, cc_data, cc_data_cnt);
-
-						last_pkg_idx = pheader.index;
-					}
-
-					if (left < cnt && left > 0)
+						if (left < cnt && left > 0)
+						{
+							/* something has been read */
+							memmove(buf, buf+cnt-left, left);
+						}
+						cnt = left;
+					}while(cc_data_cnt > 0 && cnt > 0);
+				}
+				else if(ud_format == H264_CC_TYPE)
+				{
+					aml_swap_data(buf, cnt);
+					p = buf;
+					while(cnt > 0)
 					{
-						/* something has been read */
-						memmove(buf, buf+cnt-left, left);
+						if(p[0] == 0xb5 && p[3] == 0x47 && p[4] == 0x41
+							&& p[5] == 0x39 && p[6] == 0x34)
+						{
+							min_bytes_valid = 11 + (buf[8] & 0x1F)*3;
+							//AM_DEBUG(1, "==============================================min_bytes_valid:%d", min_bytes_valid);
+							slice_type = 0;
+							ioctl(fd, AMSTREAM_IOC_UD_PICTYPE, &slice_type);
+							pic_type = aml_get_pic_type(slice_type);
+
+						    reorder.drv_data = drv_data;
+							reorder.picture_coding_type = pic_type;
+							reorder.picture_structure = FRAME_PICTURE;
+							reorder.picture_temporal_reference = 2;
+							if(cnt >= min_bytes_valid)
+								aml_reorder_user_data(&reorder, p+3, min_bytes_valid-3);//skip 0xb5 0x00 0x31
+							else
+								aml_reorder_user_data(&reorder, p+3, cnt-3);//skip 0xb5 0x00 0x31
+						}
+						cnt -= DMA_BLOCK_LEN;
+						p += DMA_BLOCK_LEN;
 					}
-					cnt = left;
-				}while(cc_data_cnt > 0 && cnt > 0);
-#else
-				dump_cc_data(buf, cnt);
-				left = 0;
-#endif
+				}
 			}
 		}
 	}
-	
-		
+
+
 	AM_DEBUG(1, "user data thread exit now");
 	return NULL;
 }
@@ -422,7 +532,7 @@ static AM_ErrorCode_t aml_open(AM_USERDATA_Device_t *dev, const AM_USERDATA_Open
 	{
 		return AM_USERDATA_ERR_NO_MEM;
 	}
-	
+
 	snprintf(dev_name, sizeof(dev_name), "/dev/amstream_userdata");
 	fd = open(dev_name, O_RDONLY);
 	if (fd == -1)
@@ -437,7 +547,7 @@ static AM_ErrorCode_t aml_open(AM_USERDATA_Device_t *dev, const AM_USERDATA_Open
 	drv_data->dev = dev;
 
 	dev->drv_data = (void*)drv_data;
-	
+
 	/* start the user data thread */
 	rc = pthread_create(&drv_data->thread, NULL, aml_userdata_thread, (void*)dev);
 	if (rc)
@@ -448,20 +558,20 @@ static AM_ErrorCode_t aml_open(AM_USERDATA_Device_t *dev, const AM_USERDATA_Open
 		AM_DEBUG(0, "%s:%s", __func__, strerror(rc));
 		return AM_USERDATA_ERR_SYS;
 	}
-	
+
 	return AM_SUCCESS;
 }
 
 static AM_ErrorCode_t aml_close(AM_USERDATA_Device_t *dev)
 {
 	aml_ud_drv_data_t *drv_data = (aml_ud_drv_data_t*)dev->drv_data;
-	
+
 	close(drv_data->fd);
 
 	drv_data->running = AM_FALSE;
 	pthread_join(drv_data->thread, NULL);
 	free(drv_data);
-	
+
 	return AM_SUCCESS;
 }
 
