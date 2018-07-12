175b7a28fa84971d205c7347a8a609b7791a0cf0
Chuangcheng Peng <chuangcheng.peng@amlogic.com>
2018-07-12 18:25:37 +0800

    DVB: frontend call dvb-frontend V5
    PD# none
    
    Change-Id: Id7eea56f3fb3931265b0f699a3c363b158ffaf79
    Signed-off-by: Chuangcheng Peng <chuangcheng.peng@amlogic.com>

--- am_adp/am_fend/linux_dvb/linux_dvb.c
+++ am_adp/am_fend/linux_dvb/linux_dvb.c
@@ -227,11 +227,16 @@ static AM_ErrorCode_t dvb_get_info (AM_FEND_Device_t *dev, struct dvb_frontend_i
 	int fd = (long)dev->drv_data;
 	int ret;
 
-	ret = ioctl(fd, FE_GET_INFO, info);
+	struct dtv_property p = {.cmd = DTV_DELIVERY_SYSTEM, .u.data = 0};
+	struct dtv_properties props = {.num = 1, .props = &p};
+
+	ret = dvb_get_prop(dev, &props);
 	if(ret != 0){
+		AM_DEBUG(1, "ioctl FE_GET_PROPERTY failed, error:%s", strerror(errno));
 		return AM_FEND_ERR_NOT_SUPPORTED;
 	}
-	switch (info->type)
+
+	switch (p.u.data)
 	{
 		case SYS_DVBS:
 			{
@@ -287,27 +292,348 @@ static AM_ErrorCode_t dvb_get_ts (AM_FEND_Device_t *dev, AM_DMX_Source_t *src)
 
 static AM_ErrorCode_t dvb_set_para (AM_FEND_Device_t *dev, const struct dvb_frontend_parameters *para)
 {
-	int fd = (long)dev->drv_data;
+	int cmd_num = 0;
+	int mode = 0;
+	int ret;
 
-	AM_DEBUG(1, "ioctl FE_SET_FRONTEND \n");
-	if (ioctl(fd, FE_SET_FRONTEND, para) == -1)
+	struct dtv_property p[DTV_IOCTL_MAX_MSGS];
+	struct dtv_properties props;
+
+	p[0].cmd = DTV_DELIVERY_SYSTEM;
+	p[0].u.data = 0;
+	props.num = 1;
+	props.props = (struct dtv_property*)&p;
+
+	ret = dvb_get_prop(dev, &props);
+	if (ret != 0) {
+		AM_DEBUG(1, "ioctl FE_GET_PROPERTY failed, error:%s", strerror(errno));
+		return AM_FEND_ERR_NOT_SUPPORTED;
+	}
+	mode = p[0].u.data;
+	cmd_num = 0;
+	p[cmd_num].cmd = DTV_FREQUENCY;
+	p[cmd_num].u.data = para->frequency;
+	cmd_num++;
+
+	p[cmd_num].cmd = DTV_INVERSION;
+	p[cmd_num].u.data = para->inversion;
+	cmd_num++;
+
+	switch (mode)
 	{
-		AM_DEBUG(1, "ioctl FE_SET_FRONTEND failed, error:%s", strerror(errno));
+		case SYS_DVBC_ANNEX_A:
+			p[cmd_num].cmd = DTV_SYMBOL_RATE;
+			p[cmd_num].u.data = para->u.qam.symbol_rate;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_MODULATION;
+			p[cmd_num].u.data = para->u.qam.modulation;
+			cmd_num++;
+			break;
+		case SYS_ATSC:
+		case SYS_DVBC_ANNEX_B:
+			p[cmd_num].cmd = DTV_MODULATION;
+			p[cmd_num].u.data = para->u.vsb.modulation;
+			cmd_num++;
+			break;
+		case SYS_DVBT:
+			p[cmd_num].cmd = DTV_BANDWIDTH_HZ;
+//			p[cmd_num].u.data = para->u.ofdm.bandwidth;
+			switch (para->u.ofdm.bandwidth) {
+			case BANDWIDTH_10_MHZ:
+				p[cmd_num].u.data = 10000000;
+				break;
+			case BANDWIDTH_8_MHZ:
+				p[cmd_num].u.data = 8000000;
+				break;
+			case BANDWIDTH_7_MHZ:
+				p[cmd_num].u.data = 7000000;
+				break;
+			case BANDWIDTH_6_MHZ:
+				p[cmd_num].u.data = 6000000;
+				break;
+			case BANDWIDTH_5_MHZ:
+				p[cmd_num].u.data = 5000000;
+				break;
+			case BANDWIDTH_1_712_MHZ:
+				p[cmd_num].u.data = 1712000;
+				break;
+			case BANDWIDTH_AUTO:
+				p[cmd_num].u.data = 0;
+			}
+			cmd_num++;
+#if 1
+			//fe_modulation_t 	constellation;
+			p[cmd_num].cmd = DTV_CODE_RATE_HP;
+			p[cmd_num].u.data = para->u.ofdm.code_rate_HP;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_CODE_RATE_LP;
+			p[cmd_num].u.data = para->u.ofdm.code_rate_LP;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_TRANSMISSION_MODE;
+			p[cmd_num].u.data = para->u.ofdm.transmission_mode;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_GUARD_INTERVAL;
+			p[cmd_num].u.data = para->u.ofdm.guard_interval;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_HIERARCHY;
+			p[cmd_num].u.data = para->u.ofdm.hierarchy_information;
+			cmd_num++;
+#endif
+			break;
+
+		case SYS_DVBS:
+			p[cmd_num].cmd = DTV_SYMBOL_RATE;
+			p[cmd_num].u.data = para->u.qpsk.symbol_rate;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_INNER_FEC;
+			p[cmd_num].u.data = para->u.qpsk.fec_inner;
+			cmd_num++;
+			break;
+
+		case SYS_DTMB:
+			p[cmd_num].cmd = DTV_BANDWIDTH_HZ;
+//			p[cmd_num].u.data = para->u.ofdm.bandwidth;
+			switch (para->u.ofdm.bandwidth) {
+			case BANDWIDTH_10_MHZ:
+				p[cmd_num].u.data = 10000000;
+				break;
+			case BANDWIDTH_8_MHZ:
+				p[cmd_num].u.data = 8000000;
+				break;
+			case BANDWIDTH_7_MHZ:
+				p[cmd_num].u.data = 7000000;
+				break;
+			case BANDWIDTH_6_MHZ:
+				p[cmd_num].u.data = 6000000;
+				break;
+			case BANDWIDTH_5_MHZ:
+				p[cmd_num].u.data = 5000000;
+				break;
+			case BANDWIDTH_1_712_MHZ:
+				p[cmd_num].u.data = 1712000;
+				break;
+			case BANDWIDTH_AUTO:
+				p[cmd_num].u.data = 0;
+			}
+			cmd_num++;
+#if 1
+			//fe_modulation_t 	constellation;
+			p[cmd_num].cmd = DTV_CODE_RATE_HP;
+			p[cmd_num].u.data = para->u.ofdm.code_rate_HP;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_CODE_RATE_LP;
+			p[cmd_num].u.data = para->u.ofdm.code_rate_LP;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_TRANSMISSION_MODE;
+			p[cmd_num].u.data = para->u.ofdm.transmission_mode;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_GUARD_INTERVAL;
+			p[cmd_num].u.data = para->u.ofdm.guard_interval;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_HIERARCHY;
+			p[cmd_num].u.data = para->u.ofdm.hierarchy_information;
+			cmd_num++;
+#endif
+			break;
+		/*now don't support below
+		case SYS_DSS:
+		case SYS_DVBS2:
+		case SYS_DVBH:
+		case SYS_ISDBT:
+		case SYS_ISDBS:
+		case SYS_ISDBC:
+		case SYS_ATSCMH:
+		case SYS_CMMB:
+		case SYS_DAB:
+		case SYS_DVBT2:
+		case SYS_TURBO:
+		case SYS_DVBC_ANNEX_C:*/
+		default:
+			AM_DEBUG(1, "delivery system:%d not support error",mode);
+			return AM_FEND_ERR_NOT_SUPPORTED;
+	}
+
+	p[cmd_num].cmd = DTV_TUNE;
+	cmd_num++;
+
+	props.num = cmd_num;
+	props.props = (struct dtv_property *)&p;
+
+	ret = dvb_set_prop(dev, &props);
+	if (ret != 0) {
+		AM_DEBUG(1, "set tune failed\n");
 		return AM_FAILURE;
 	}
-	AM_DEBUG(1, "ioctl FE_SET_FRONTEND success \n");
-
 	return AM_SUCCESS;
 }
 
 static AM_ErrorCode_t dvb_get_para (AM_FEND_Device_t *dev, struct dvb_frontend_parameters *para)
 {
-	int fd = (long)dev->drv_data;
-	
-	if(ioctl(fd, FE_GET_FRONTEND, para)==-1)
+	int cmd_num = 0;
+	int mode = 0;
+	int ret = 0;
+	struct dtv_property p[DTV_IOCTL_MAX_MSGS];
+	struct dtv_properties props;
+
+	p[0].cmd = DTV_DELIVERY_SYSTEM;
+	p[0].u.data = 0;
+	props.num = 1;
+	props.props = (struct dtv_property *)&p;
+
+	ret = dvb_get_prop(dev, &props);
+	if (ret != 0) {
+		AM_DEBUG(1, "dvb_get_prop failed, error:%s", strerror(errno));
+		return AM_FEND_ERR_NOT_SUPPORTED;
+	}
+	mode = p[0].u.data;
+
+	cmd_num = 0;
+	p[cmd_num].cmd = DTV_FREQUENCY;
+	cmd_num++;
+	p[cmd_num].cmd = DTV_INVERSION;
+	cmd_num++;
+	switch (mode)
 	{
-		AM_DEBUG(1, "ioctl FE_GET_FRONTEND failed, error:%s", strerror(errno));
+		case SYS_DVBC_ANNEX_A:
+			p[cmd_num].cmd = DTV_SYMBOL_RATE;
+			cmd_num++;
+			p[cmd_num].cmd = DTV_MODULATION;
+			cmd_num++;
+
+			break;
+		case SYS_ATSC:
+		case SYS_DVBC_ANNEX_B:
+			p[cmd_num].cmd = DTV_MODULATION;
+			cmd_num++;
+			break;
+		case SYS_DVBT:
+		case SYS_DTMB:
+			p[cmd_num].cmd = DTV_BANDWIDTH_HZ;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_CODE_RATE_HP;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_CODE_RATE_LP;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_TRANSMISSION_MODE;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_GUARD_INTERVAL;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_HIERARCHY;
+			cmd_num++;
+			break;
+
+		case SYS_DVBS:
+			p[cmd_num].cmd = DTV_SYMBOL_RATE;
+			p[cmd_num].u.data = para->u.qpsk.symbol_rate;
+			cmd_num++;
+
+			p[cmd_num].cmd = DTV_INNER_FEC;
+			p[cmd_num].u.data = para->u.qpsk.fec_inner;
+			cmd_num++;
+			break;
+		/*now don't support
+		case SYS_DSS:
+		case SYS_DVBS2:
+		case SYS_DVBH:
+		case SYS_ISDBT:
+		case SYS_ISDBS:
+		case SYS_ISDBC:
+		case SYS_ATSCMH:
+		case SYS_CMMB:
+		case SYS_DAB:
+		case SYS_DVBT2:
+		case SYS_TURBO:
+		case SYS_DVBC_ANNEX_C:*/
+		default:
+			AM_DEBUG(1, "delivery system:%d not support error",mode);
+			return AM_FEND_ERR_NOT_SUPPORTED;
+	}
+
+	props.num = cmd_num;
+	props.props = (struct dtv_property *)&p;
+
+	ret = dvb_get_prop(dev, &props);
+	if (ret != 0) {
+		AM_DEBUG(1, "set tune failed\n");
 		return AM_FAILURE;
+	}
+
+	cmd_num = 0;
+	para->frequency = p[cmd_num].u.data;
+	cmd_num++;
+	para->inversion = p[cmd_num].u.data;
+	cmd_num++;
+
+	switch (mode)
+	{
+		case SYS_DVBC_ANNEX_A:
+			para->u.qam.symbol_rate = p[cmd_num].u.data;
+			cmd_num++;
+			para->u.qam.modulation = p[cmd_num].u.data;
+
+			break;
+		case SYS_ATSC:
+		case SYS_DVBC_ANNEX_B:
+			para->u.vsb.modulation = p[cmd_num].u.data;
+			break;
+		case SYS_DVBT:
+		case SYS_DTMB:
+			switch (p[cmd_num].u.data) {
+			case 10000000:
+				para->u.ofdm.bandwidth = BANDWIDTH_10_MHZ;
+				break;
+			case 8000000:
+				para->u.ofdm.bandwidth = BANDWIDTH_8_MHZ;
+				break;
+			case 7000000:
+				para->u.ofdm.bandwidth = BANDWIDTH_7_MHZ;
+				break;
+			case 6000000:
+				para->u.ofdm.bandwidth = BANDWIDTH_6_MHZ;
+				break;
+			case 5000000:
+				para->u.ofdm.bandwidth = BANDWIDTH_5_MHZ;
+				break;
+			case 1712000:
+				para->u.ofdm.bandwidth = BANDWIDTH_1_712_MHZ;
+				break;
+			case 0:
+				para->u.ofdm.bandwidth = BANDWIDTH_AUTO;
+			}
+//			para->u.ofdm.bandwidth = p[cmd_num].u.data;
+			cmd_num++;
+			para->u.ofdm.code_rate_HP = p[cmd_num].u.data;
+			cmd_num++;
+			para->u.ofdm.code_rate_LP = p[cmd_num].u.data;
+			cmd_num++;
+			para->u.ofdm.transmission_mode = p[cmd_num].u.data;
+			cmd_num++;
+			para->u.ofdm.guard_interval = p[cmd_num].u.data;
+			cmd_num++;
+			para->u.ofdm.hierarchy_information = p[cmd_num].u.data;
+			break;
+
+		case SYS_DVBS:
+			para->u.qpsk.symbol_rate = p[cmd_num].u.data;
+			cmd_num++;
+
+			para->u.qpsk.fec_inner = p[cmd_num].u.data;
+			break;
 	}
 
 	return AM_SUCCESS;
@@ -329,7 +655,7 @@ static AM_ErrorCode_t dvb_set_prop (AM_FEND_Device_t *dev, const struct dtv_prop
 static AM_ErrorCode_t dvb_get_prop (AM_FEND_Device_t *dev, struct dtv_properties *prop)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_GET_PROPERTY, prop)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_GET_PROPERTY failed, error:%s", strerror(errno));
@@ -356,7 +682,7 @@ static AM_ErrorCode_t dvb_get_snr (AM_FEND_Device_t *dev, int *snr)
 {
 	int fd = (long)dev->drv_data;
 	uint16_t v16;
-	
+
 	if(ioctl(fd, FE_READ_SNR, &v16)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_READ_SNR failed, error:%s", strerror(errno));
@@ -371,7 +697,7 @@ static AM_ErrorCode_t dvb_get_ber (AM_FEND_Device_t *dev, int *ber)
 {
 	int fd = (long)dev->drv_data;
 	uint32_t v32;
-	
+
 	if(ioctl(fd, FE_READ_BER, &v32)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_READ_BER failed, error:%s", strerror(errno));
@@ -386,7 +712,7 @@ static AM_ErrorCode_t dvb_get_strength (AM_FEND_Device_t *dev, int *strength)
 {
 	int fd = (long)dev->drv_data;
 	uint16_t v16;
-	
+
 	if(ioctl(fd, FE_READ_SIGNAL_STRENGTH, &v16)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_READ_SIGNAL_STRENGTH failed, error:%s", strerror(errno));
@@ -405,13 +731,13 @@ static AM_ErrorCode_t dvb_wait_event (AM_FEND_Device_t *dev, struct dvb_frontend
 
 	pfd.fd = fd;
 	pfd.events = POLLIN;
-	
+
 	ret = poll(&pfd, 1, timeout);
 	if(ret!=1)
 	{
 		return AM_FEND_ERR_TIMEOUT;
 	}
-	
+
 	if (ioctl(fd, FE_GET_EVENT, evt) == -1)
 	{
 		AM_DEBUG(1, "ioctl FE_GET_EVENT failed, error:%s", strerror(errno));
@@ -424,7 +750,7 @@ static AM_ErrorCode_t dvb_wait_event (AM_FEND_Device_t *dev, struct dvb_frontend
 static AM_ErrorCode_t dvb_set_delay(AM_FEND_Device_t *dev,  int delay)
 {
 	int fd = (long)dev->drv_data;
-	
+
 #ifdef FE_SET_DELAY
 	if(ioctl(fd, FE_SET_DELAY, delay)==-1)
 	{
@@ -438,20 +764,20 @@ static AM_ErrorCode_t dvb_set_delay(AM_FEND_Device_t *dev,  int delay)
 static AM_ErrorCode_t dvb_diseqc_reset_overload(AM_FEND_Device_t *dev)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_DISEQC_RESET_OVERLOAD, 0)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_DISEQC_RESET_OVERLOAD failed, error:%s", strerror(errno));
 		return AM_FAILURE;
 	}
 
-	return AM_SUCCESS;	
+	return AM_SUCCESS;
 }
 
 static AM_ErrorCode_t dvb_diseqc_send_master_cmd(AM_FEND_Device_t *dev, struct dvb_diseqc_master_cmd* cmd)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_DISEQC_SEND_MASTER_CMD, cmd)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_DISEQC_SEND_MASTER_CMD failed, error:%s", strerror(errno));
@@ -464,7 +790,7 @@ static AM_ErrorCode_t dvb_diseqc_send_master_cmd(AM_FEND_Device_t *dev, struct d
 static AM_ErrorCode_t dvb_diseqc_recv_slave_reply(AM_FEND_Device_t *dev, struct dvb_diseqc_slave_reply* reply)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_DISEQC_RECV_SLAVE_REPLY, reply)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_DISEQC_RECV_SLAVE_REPLY failed, error:%s", strerror(errno));
@@ -477,61 +803,61 @@ static AM_ErrorCode_t dvb_diseqc_recv_slave_reply(AM_FEND_Device_t *dev, struct 
 static AM_ErrorCode_t dvb_diseqc_send_burst(AM_FEND_Device_t *dev, fe_sec_mini_cmd_t minicmd)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_DISEQC_SEND_BURST, minicmd)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_DISEQC_SEND_BURST failed, error:%s", strerror(errno));
 		return AM_FAILURE;
 	}
 
-	return AM_SUCCESS;	
+	return AM_SUCCESS;
 }
 
 static AM_ErrorCode_t dvb_set_tone(AM_FEND_Device_t *dev, fe_sec_tone_mode_t tone)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_SET_TONE, tone)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_SET_TONE failed, error:%s", strerror(errno));
 		return AM_FAILURE;
 	}
 
-	return AM_SUCCESS;	
+	return AM_SUCCESS;
 }
 
 static AM_ErrorCode_t dvb_set_voltage(AM_FEND_Device_t *dev, fe_sec_voltage_t voltage)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_SET_VOLTAGE, voltage)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_SET_VOLTAGE failed, error:%s", strerror(errno));
 		return AM_FAILURE;
 	}
 
-	return AM_SUCCESS;	
+	return AM_SUCCESS;
 }
 
 static AM_ErrorCode_t dvb_enable_high_lnb_voltage(AM_FEND_Device_t *dev, long arg)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_ENABLE_HIGH_LNB_VOLTAGE, arg)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_ENABLE_HIGH_LNB_VOLTAGE failed, error:%s", strerror(errno));
 		return AM_FAILURE;
 	}
 
-	return AM_SUCCESS;	
+	return AM_SUCCESS;
 }
 
 static AM_ErrorCode_t dvb_close (AM_FEND_Device_t *dev)
 {
 	int fd = (long)dev->drv_data;
-	
+
 	close(fd);
-	
+
 	return AM_SUCCESS;
 }
 
@@ -540,7 +866,7 @@ static AM_ErrorCode_t dvbsx_blindscan_scan(AM_FEND_Device_t *dev, struct dvbsx_b
 	int ret =AM_SUCCESS;
 	#if 0
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_SET_BLINDSCAN, pbspara)==-1)
 	{
 		AM_DEBUG(1, "ioctl dvbsx_blindscan_scan failed, error:%s", strerror(errno));
@@ -591,18 +917,18 @@ static AM_ErrorCode_t dvbsx_blindscan_scan(AM_FEND_Device_t *dev, struct dvbsx_b
 	}
 	if (property != NULL) {
 		free(property);
-		property = NULL;	
+		property = NULL;
 	}
 	#endif
-	return ret;	
+	return ret;
 }
 
 static AM_ErrorCode_t dvbsx_blindscan_getscanevent(AM_FEND_Device_t *dev, struct dvbsx_blindscanevent *pbsevent)
 {
 	int ret = AM_SUCCESS;
 	#if 0
-	int fd = (long)dev->drv_data;	
-	
+	int fd = (long)dev->drv_data;
+
 	if(ioctl(fd, FE_GET_BLINDSCANEVENT, pbsevent)!=0)
 	{
 		AM_DEBUG(1, "ioctl FE_GET_BLINDSCANEVENT failed, error:%s", strerror(errno));
@@ -617,14 +943,14 @@ static AM_ErrorCode_t dvbsx_blindscan_getscanevent(AM_FEND_Device_t *dev, struct
 		pbsevent->u.m_uistartfreq_khz = event.parameters.frequency;
 	} else if (event.status&BLINDSCAN_UPDATEPROCESS) {
 		pbsevent->status = BLINDSCAN_UPDATEPROCESS;
-		pbsevent->u.m_uiprogress = event.parameters.frequency;	
+		pbsevent->u.m_uiprogress = event.parameters.frequency;
 	} else if (BLINDSCAN_UPDATERESULTFREQ) {
 		pbsevent->status = BLINDSCAN_UPDATERESULTFREQ;
 	    memcpy(&(pbsevent->u.parameters),
-				&(event.parameters), sizeof(struct dvb_frontend_parameters));	
+				&(event.parameters), sizeof(struct dvb_frontend_parameters));
 	}
 	#endif
-	return ret;	
+	return ret;
 }
 
 static AM_ErrorCode_t dvbsx_blindscan_cancel(AM_FEND_Device_t *dev)
@@ -632,7 +958,7 @@ static AM_ErrorCode_t dvbsx_blindscan_cancel(AM_FEND_Device_t *dev)
 	int ret = AM_SUCCESS;
 	#if 0
 	int fd = (long)dev->drv_data;
-	
+
 	if(ioctl(fd, FE_SET_BLINDSCANCANCEl)==-1)
 	{
 		AM_DEBUG(1, "ioctl FE_SET_BLINDSCANCANCEl failed, error:%s", strerror(errno));
@@ -655,7 +981,7 @@ static AM_ErrorCode_t dvbsx_blindscan_cancel(AM_FEND_Device_t *dev)
 		AM_DEBUG(1, "set cancel blind scan error\n");
 	}
 	#endif
-	return ret;	
+	return ret;
 }
 
 static AM_ErrorCode_t dvb_fine_tune(AM_FEND_Device_t *dev, unsigned int freq)
