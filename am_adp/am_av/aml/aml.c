1042cf33579db43aa8fa3359236da43c12bcf004
Yinming Ding <yinming.ding@amlogic.com>
2019-03-21 15:33:17 +0800

    dvb: am_adp: set tsync_mode as AMASTER on DRA fmt [1/1]
    PD#SWPL-5561
    
    Problem:
    There is no sound 0-2s before repeating playback on DRA test stream
    
    Solution:
    Set tsync_mode as AMASTER on DRA fmt
    
    Verify:
    Verified by X301
    
    Change-Id: I34fc35e72a453116b159bd6c05ba7531fe637228
    Signed-off-by: Yinming Ding <yinming.ding@amlogic.com>

--- am_adp/am_av/aml/aml.c
+++ am_adp/am_av/aml/aml.c
@@ -3846,6 +3846,9 @@ static int aml_calc_sync_mode(AM_AV_Device_t *dev, int has_audio, int has_video,
 	if ((tsync_mode == AMASTER) && !has_audio)
 		tsync_mode = VMASTER;
 
+	if (afmt == AFORMAT_DRA && has_audio)
+		tsync_mode = AMASTER;
+
 	return tsync_mode;
 }
 
