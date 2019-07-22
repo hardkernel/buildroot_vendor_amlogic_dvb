db5b701045cfb41b4d5d659b362d7ea5edec2d2d
Chuanzhi Wang <chuanzhi.wang@amlogic.com>
2019-07-22 21:00:55 +0800

    dvb:  add LICENSE for am_adp. [1/1]
    PD#SWPL-11540
    
    Problem:
    Adjust the LICENSE under am_adp.
    
    Solution:
    add it.
    
    Verify:
    on R314
    
    Change-Id: I1deec8bbc14b7b698d94fa68780531d8fb3f613b

--- am_adp/am_ad/am_ad.c
+++ am_adp/am_ad/am_ad.c
@@ -1,15 +1,8 @@
 /*
- * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
+ * Copyright (c) 2014 Amlogic, Inc. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
  *
  * Description: 
 */
--- include/am_adp/am_userdata.h
+++ include/am_adp/am_userdata.h
@@ -1,19 +1,12 @@
 /***************************************************************************
- * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
+ * Copyright (c) 2014 Amlogic, Inc. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
  *
  * Description:
  */
-/**\file am_userdata.h
+ /**\file am_userdata.h
  * \brief MPEG user data device module
  *
  * \author Xia Lei Peng <leipeng.xia@amlogic.com>
